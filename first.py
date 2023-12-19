09953918d54b1198c41a2ab39389c3cebb956ab4
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-12-19 20:18:18 +0900

    Retrun Recap

--- first.py
+++ first.py
@@ -1,1 +1,14 @@
-print("hello from a file")
+def make_juice(fruit):
+    return f"{fruit}+🍹"
+    
+def add_ice(juice):
+    return f"{juice}+🧊"
+
+def add_suger(iced_juice):
+    return f"{iced_juice}+🍬"
+
+juice = make_juice("🍎")
+cold_juice = add_ice(juice)
+perfect_juice = add_suger(cold_juice)
+
+print(perfect_juice)
