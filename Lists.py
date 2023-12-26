80c81f0de3ae49e552e70b0705c3a8ac39b7d810
HaHyunkyung <87869664+HaHyunkyung@users.noreply.github.com>
2023-12-26 18:18:27 +0900

    feat: Lists

---
+++ Lists.py
@@ -0,0 +1,13 @@
+days_of_week = ["Mon","Tue","Wed","Thur","Fri"]
+
+print(days_of_week.count("Wed"))
+print(days_of_week)
+days_of_week.append("Sat")
+print(days_of_week)
+days_of_week.append("Sun")
+print(days_of_week)
+days_of_week.reverse()
+print(days_of_week)
+days_of_week.remove("Mon")
+print(days_of_week)
+print(days_of_week[3])
